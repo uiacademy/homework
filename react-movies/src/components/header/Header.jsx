e66976cdc79fddfe9e3c51715e2d1670183b22ee
Codegem <evaldaskis@gmail.com>
2021-02-07 02:23:13 +0200

    need to return selected movie to input

--- react-movies/src/components/header/Header.jsx
+++ react-movies/src/components/header/Header.jsx
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import {ReactComponent as Movie} from './icons/movie.svg'
 import {ReactComponent as Search} from './icons/search.svg'
 import SearchRez from '../search/SearchResults'
 
-export default function Header() {
+export default function Header(props) {
 
   const BASE_URL = 'https://api.themoviedb.org/3/';
   const APIKEY = '68ae61f2720168696df123e0c841e5d8';
-  const [searchText, setsearchText] = useState([]);
+
+  const [searchText, setsearchText] = useState('');
   const [data, setdata] = useState([]);
+  const [inputLength, setinputLength] = useState(1);
 
   const inputHandler = (e) => {
-    const text = e.target.value
+    const text = e.target.value;
     setsearchText(text)
-    if(text !== ''){
+    setinputLength(text.length);
+    if(text !== '' ){
       let url = ''.concat(BASE_URL, 'search/movie?api_key=', APIKEY, '&query=', text);
       fetch(url)
       .then((result) => {
@@ -23,22 +26,29 @@ export default function Header() {
         setdata(data.results);
       })
       .catch(function(error){
-
+        console.log('something went completely wrong')
+        return; // atvirai tai nezinau ar padetu nes error negavau ^^
       })
     }
   }
-
+  
   return (
     <div>
       <header>
         <div className="search-content">
           <div className="search-wrap">
             <Movie className="movie-icon"/>
-            <input type="text" placeholder="Enter Movie Name" id="search" onChange={inputHandler} value={searchText} />
+            <input 
+              type="text" 
+              placeholder="Enter Movie Name" 
+              id="search" 
+              onChange={inputHandler}
+              value={searchText} 
+            />
           </div>
           <Search className="search-icon"/>
         </div>
-          <SearchRez data={data}/>
+          <SearchRez data={data} inputLength={inputLength}/>
       </header>
     </div>
   )
--- react-movies/src/components/search/SearchResults.jsx
+++ react-movies/src/components/search/SearchResults.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 
 export default function SearchResults(props) {
-  const data = props.data.slice(0, 10);
+
+  const data = props.data.slice(0, 5);
+  const inputLength = props.inputLength;
+  
+  const selectorHandler = (id) => {
+    console.log(id)    
+    return id;
+  }
+  
+  if(inputLength >= 3){
   return (
-    <div>
+    <div className="results">
         {data.map((data, index) => {
           return (
-            <div className="results" key={index}>
-              <ul key={index}>
-                <li key={index}>{data.original_title}
-                <span>{data.vote_average} Rating, {data.release_date.slice(0, 4)}</span></li>
+            <div className="results__list" key={index}>
+              <ul key={index} id={data.id} onClick={() => selectorHandler(data.original_title)}>
+                <li key={index}>{data.original_title}</li><br/>
+                <span>{data.vote_average} Rating, {data.release_date.slice(0, 4)}</span>
               </ul>
             </div>
           )
         })}
     </div>
   )
+  }else{
+    return null;
+  }
 }
